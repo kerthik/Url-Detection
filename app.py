0bd3447d03e3e756395fd0d889319d36e5750adb
Kerthik  S <kerthikcyc@gmail.com>
2024-12-27 19:16:22 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import warnings
+import pickle
+from convert import convertion
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+file = open("newmodel.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+app = Flask(__name__)
+#from flask import Flask, render_template, request
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route('/result',methods=['POST','GET'])
+def predict():
+    if request.method == "POST":
+        url = request.form["name"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30)
+    
+        y_pred =gbc.predict(x)[0]
+           
+        name=convertion(url,int(y_pred))
+        return render_template("index.html", name=name)
+@app.route('/usecases', methods=['GET', 'POST'])
+def usecases():
+    return render_template('usecases.html')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
